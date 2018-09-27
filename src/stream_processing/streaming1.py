5b41cd2615b6dc3496979c95c5df1b27ffebe6d1
nayoonwoo8899 <nayoonwoo8899@gmail.com>
2018-09-26 20:50:47 -0400

    add kafka streaming

---
+++ src/stream_processing/streaming1.py
@@ -0,0 +1,85 @@
+import threading
+import time
+import json
+import redis
+from kafka import KafkaConsumer
+import datetime
+import pyspark_cassandra
+from pyspark_cassandra import CassandraSparkContext, saveToCassandra
+
+
+
+class Streaming(threading.Thread):
+    daemon = True
+
+    # Constructor sets up Redis connection and algorithm vars
+    def __init__(self):
+        super(Streaming, self).__init__()
+
+        # Set up connection to Redis server
+        self.redis_server = 'localhost'
+        self.redis_db = redis.StrictRedis(host=self.redis_server, port=6379, db=0)
+
+        # intiailize internal variables
+
+    # Thread sets up consumer and consumes kafka messages
+    def run(self):
+        consumer = KafkaConsumer(bootstrap_servers='localhost:9092')
+        consumer.subscribe(['venmo-transactions'])
+
+        for message in consumer:
+            msg = str(message.value)
+            self.analyze_message(msg)
+
+    # Assign colors to message based on emoji/text content
+    def analyze_message(self, json_obj):
+        
+        json_data = json.loads(json_obj)
+        #from_id = json_data['actor']['id']
+        #to_id = json_data['transactions'][0]['target']['id']
+        timestamp = json_data['created_time']
+        year=int(timestamp[0:4])
+        month = int(timestamp[5:7])
+        date=int(timestamp[8:10])
+        hour=int(timestamp[11:13])
+        dayofweek=int(datetime.datetime(year, month, date).strftime('%w'))
+       
+        # read previous value from Redis        
+        prev_hour=self.redis_db.get('hour')
+        counting=self.redis_db.get('counting')
+
+
+
+        if prev_hour==None or prev_hour!=hour:
+            print('year=',year,'month=',month,'date=',date,'day of week=',dayofweek,'hour=',hour, '# of transaction during past 1 hour=', counting)
+            self.redis_db.set('year',year)
+            self.redis_db.set('month',month)
+            self.redis_db.set('date',date)
+            self.redis_db.set('day of week',dayofweek)
+            self.redis_db.set('hour',hour)
+            self.redis_db.set('counting',1)
+        
+        else:
+
+            self.redis_db.set('year',year)
+            self.redis_db.set('month',month)
+            self.redis_db.set('date',date)
+            self.redis_db.set('day of week',dayofweek)
+            self.redis_db.set('hour',hour)
+            self.redis_db.set('counting',counting+1)
+            
+       
+                 
+if __name__ == "__main__":
+    counting=0
+    thread = Streaming()
+    
+    while True:
+        if not thread.isAlive():
+            print("Starting Kafka consumer...")
+            thread.start()
+            print("Started Kafka consumer.")
+        else:
+            print("Listening for new messages in topic: 'venmo-transactions'...")
+time.sleep(0.1)
+
---
+++ src/stream_processing/streaming_sparkstreaming.py
@@ -0,0 +1,115 @@
+import sys
+import json
+from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
+from pyspark.sql import SparkSession
+from pyspark.streaming.kafka import KafkaUtils
+from pyspark.sql import Row
+
+from pyspark.sql.functions import unix_timestamp
+import time
+from pyspark.sql.functions import udf
+from pyspark.sql.types import StringType
+from pyspark.sql.functions import regexp_replace, col
+from pyspark.sql.functions import *
+from datetime import datetime
+import redis
+
+redis_server = 'localhost'
+redis_db = redis.StrictRedis(host=self.redis_server, port=6379, db=0)
+
+
+
+"""
+artist_list = set()
+with open('../data/lower-artist.csv', 'r') as f:
+    count =1
+    for i in f:
+        cur = i.split("\n")[0]
+        if count in range(100):
+            artist_list.add(cur)
+        count+=1
+
+def getSparkSessionInstance(sparkConf):
+    if ("sparkSessionSingletonInstance" not in globals()):
+        globals()["sparkSessionSingletonInstance"] = SparkSession.builder.config(conf=sparkConf).getOrCreate()
+    return globals()["sparkSessionSingletonInstance"]
+
+
+@udf('string')
+
+def func(text):
+    for word in artist_list:
+        if word in text.lower():
+            return word
+
+@udf('string')
+
+def transfer_time(text):
+    #return "2018-06-25"
+    return datetime.today().strftime("%Y-%m-%d %H:%M:%S")
+"""
+
+
+def process(json_obj):
+    json_data = json.loads(json_obj)
+
+    timestamp = json_data['created_time']
+    year = int(timestamp[0:4])
+    month = int(timestamp[5:7])
+    date = int(timestamp[8:10])
+    hour = int(timestamp[11:13])
+    dayofweek = int(datetime.datetime(year, month, date).strftime('%w'))
+
+    # read previous value from Redis
+    prev_hour = redis_db.get('hour')
+    counting = redis_db.get('counting')
+
+    if prev_hour == None or prev_hour != hour:
+        print('year=', year, 'month=', month, 'date=', date, 'day of week=', dayofweek, 'hour=', hour,
+              '# of transaction during past 1 hour=', counting)
+        redis_db.set('year', year)
+        redis_db.set('month', month)
+        redis_db.set('date', date)
+        redis_db.set('day of week', dayofweek)
+        redis_db.set('hour', hour)
+        redis_db.set('counting', 1)
+
+    else:
+
+        redis_db.set('year', year)
+        redis_db.set('month', month)
+        redis_db.set('date', date)
+        redis_db.set('day of week', dayofweek)
+        redis_db.set('hour', hour)
+        redis_db.set('counting', counting + 1)
+
+"""
+def process(rdd):
+    spark = getSparkSessionInstance(rdd.context.getConf())
+    venmoDataFrame = spark.read.json(rdd)
+    df = venmoDataFrame.withColumn('hashtag', func(tweetsDataFrame.text))
+    df = df.withColumn('time',transfer_time(tweetsDataFrame.time))
+    df.createOrReplaceTempView("historicaltweets")
+    df = spark.sql("SELECT MAX(time) AS time,hashtag, count(*) AS count FROM historicaltweets WHERE hashtag IS NOT NULL GROUP BY hashtag ORDER BY count DESC")
+    rdd = df.rdd.map(tuple)
+    rdd.saveToCassandra("twitter","tweet")
+    df.show()
+"""
+
+
+if __name__ == "__main__":
+    sc = SparkContext(appName="venmo-transactions-app")
+    #sc.setLogLevel("WARN")
+    ssc = StreamingContext(sc,1)
+    topic_name = "venmo-transactions"
+    brokers_dns_str = "0.0.0.0:9092"
+
+    streamFromKafka = KafkaUtils.createDirectStream(ssc, [topic_name],{"metadata.broker.list":brokers_dns_str})
+    lines = streamFromKafka.map(lambda x: x[1])
+
+    lines.count().pprint()
+    lines.foreachRDD(process)
+    #text_counts = lines.map(lambda tweet: (tweet['hashtag'],1)).reduceByKey(lambda x,y: x + y)
+    ssc.start()
+    ssc.awaitTermination()
