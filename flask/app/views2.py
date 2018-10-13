3f30be80cf52651f6617ee03f6e42ddf561af0ee
nayoonwoo8899 <nayoonwoo8899@gmail.com>
2018-10-12 23:41:34 -0400

    new user batch code

---
+++ flask/app/views2.py
@@ -0,0 +1,306 @@
+from app import app
+from flask_mysqldb import MySQL
+from flask import render_template, request, jsonify
+import time
+import datetime
+import redis
+import pandas as pd
+from bokeh.plotting import figure
+from bokeh.models import ColumnDataSource
+from bokeh.models import AjaxDataSource
+from bokeh.embed import components
+from bokeh.core.properties import value
+from bokeh.io import show, output_file
+#from bokeh.charts import Bar
+
+
+# Configuration of MySQL db
+app.config['MYSQL_HOST'] = 'ec2-54-82-188-230.compute-1.amazonaws.com'
+app.config['MYSQL_USER'] = 'nayoon'
+app.config['MYSQL_PASSWORD'] = 'haonayoon'
+app.config['MYSQL_DB'] = 'insight_data_2'
+
+mysql = MySQL(app)
+
+
+@app.route('/ventra2/batch_result/year/', methods=['GET'])
+def batch_hour():
+
+
+    cur = mysql.connection.cursor()
+    resultvalue = cur.execute("select year, sum(count) from Data_year_dayofweek_hour where year < 2017 group by year ")
+    if resultvalue:
+        details = cur.fetchall()
+        cur.close()
+        df = pd.read_sql("select year, sum(count) from Data_year_dayofweek_hour where year < 2017 group by year ", con=mysql.connection)
+        source = ColumnDataSource(df)
+        plot = figure(plot_width=500, plot_height=400, x_axis_label='year', y_axis_label='count')
+
+        plot.vbar(x='year', source=source, width=1, top='sum(count)', fill_color="orangered", fill_alpha=0.5,
+                  line_color="orangered")
+        script, div = components(plot)
+        return render_template('second_year.html', details=details, script=script, div=div)
+    else:
+        cur.close()
+
+
+
+
+
+
+@app.route('/ventra2/batch_result/year_hour/', methods=['GET'])
+def batch_year_hour():
+
+
+    cur = mysql.connection.cursor()
+    resultvalue = cur.execute("select year,hour,sum(count) from Data_year_dayofweek_hour group by hour,year")
+
+    if resultvalue:
+        details = cur.fetchall()
+        cur.close()
+        df = pd.read_sql("select year,hour,sum(count) from Data_year_dayofweek_hour group by hour,year", con=mysql.connection)
+        source = ColumnDataSource(df)
+
+
+        plot = figure(plot_width=500, plot_height=400, x_axis_label='hour', y_axis_label='count')
+
+        plot.vbar_stack('year',x='hour',source=source ,width=0.5,color=['red','salmon','orange','yellow','green','blue','pink'])
+        #color=['red','salmon','orange','yellow','green','blue','pink']
+
+
+        script, div = components(plot)
+
+        return render_template('second_year_hour.html', details=details, script=script, div=div)
+    else:
+        cur.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+@app.route('/ventra2/batch_result/year_hour',methods=['GET'])
+def batch_hour():
+    cur = mysql.connection.cursor()
+    resultvalue=cur.execute("select * from Data_2015_Ver1")
+    if resultvalue:
+        details=cur.fetchall()
+        cur.close()
+        df = pd.read_sql('SELECT * FROM Data_2015_Ver1', con=mysql.connection)
+        source = ColumnDataSource(df)
+        plot = figure(plot_width=500, plot_height=400, x_axis_label='hour', y_axis_label='count', y_range=(0, 3.2*10**6),x_range=(-1, 24))
+
+        plot.vbar(x = 'hour', source = source, width = 1, top = 'count',fill_color="orangered",fill_alpha=0.5,line_color="orangered")
+        script, div = components(plot)
+        return render_template('batch_hour.html', details=details, script = script, div = div)
+    else:
+        cur.close()
+
+
+
+@app.route('/ventra2/batch_result/year_dayofweek/',methods=['GET'])
+def batch_dayofweek():
+
+    cur = mysql.connection.cursor()
+    resultvalue = cur.execute("select dayofweek,sum(count) from Data_2015_Ver1_dayofweek group by dayofweek")
+    if resultvalue:
+        details = cur.fetchall()
+        cur.close()
+        df = pd.read_sql("SELECT dayofweek,sum(count) FROM Data_2015_Ver1_dayofweek GROUP BY dayofweek", con=mysql.connection)
+        labels = []
+        for dow in df['dayofweek']:
+            if dow == 0:
+                labels.append('Monday')
+            elif dow == 1:
+                labels.append('Tuesday')
+            elif dow == 2:
+                labels.append('Wednesday')
+            elif dow == 3:
+                labels.append('Thursday')
+            elif dow == 4:
+                labels.append('Friday')
+            elif dow == 5:
+                labels.append('Saturday')
+            else:
+                labels.append('Sunday')
+        df['label'] = labels
+        #df.iloc[:, -1] = df.iloc[:, -1].div(52)
+        #df.iloc[-4, -1] = df.iloc[-4, -1].multiply(52).divide(53)
+        source = ColumnDataSource(df)
+        plot = figure(plot_width=500, plot_height=400, x_axis_label='dayofweek', y_axis_label='count', y_range=(0.55*10**7, 0.75*10**7),x_range=labels)
+        plot.vbar(x = 'label', source = source, width =1, top = 'sum(count)', fill_alpha=0.5)
+        script, div = components(plot)
+        return render_template('batch_dow.html', details = details, script = script, div = div)
+    else:
+        cur.close()
+
+
+@app.route('/ventra/batch_result/dayofweek_by_hour/', methods = ['GET', 'POST'])
+def batch_dayofweek_by_hour():
+    hours = list(range(24))
+    hour = request.args.get('hour')
+    if hour == None:
+        hour = '0'
+    cur = mysql.connection.cursor()
+    resultvalue = cur.execute("select * from Data_2015_Ver1_dayofweek where hour = " + hour)
+    if resultvalue:
+        details = cur.fetchall()
+        cur.close()
+        df = pd.read_sql("SELECT * FROM Data_2015_Ver1_dayofweek WHERE hour = " + hour,con=mysql.connection)
+        labels = []
+        for dow in df['dayofweek']:
+            if dow == 0:
+                labels.append('Monday')
+            elif dow == 1:
+                labels.append('Tuesday')
+            elif dow == 2:
+                labels.append('Wednesday')
+            elif dow == 3:
+                labels.append('Thursday')
+            elif dow == 4:
+                labels.append('Friday')
+            elif dow == 5:
+                labels.append('Saturday')
+            else:
+                labels.append('Sunday')
+        df['label'] = labels
+        source = ColumnDataSource(df)
+        plot = figure(plot_width=500, plot_height=400,x_axis_label='hour', y_axis_label='count',y_range=(0, 5*10**5),x_range = labels)
+        plot.vbar(x='label', source=source, width=1, top='count',fill_alpha=0.5)
+        script, div = components(plot)
+        return render_template('batch_dow_by_hour.html', details=details, current_hour = int(hour), hours = hours, script=script, div=div)
+    else:
+        cur.close()
+
+
+@app.route('/ventra/batch_result/dayofweek_by_dow/', methods = ['GET', 'POST'])
+def batch_dayofweek_by_dow():
+    dows = list(range(7))
+    dow = request.args.get('dow')
+    if dow == None:
+        dow = '0'
+    cur = mysql.connection.cursor()
+    resultvalue = cur.execute("select * from Data_2015_Ver1_dayofweek where dayofweek= " + dow)
+    if resultvalue:
+        details = cur.fetchall()
+        cur.close()
+        df = pd.read_sql("select * from Data_2015_Ver1_dayofweek where dayofweek = " + dow,con=mysql.connection)
+        source = ColumnDataSource(df)
+        plot = figure(plot_width=500, plot_height=400,x_axis_label='hour', y_axis_label='count')
+        plot.vbar(x='hour', source=source, width=1, top='count',fill_alpha=0.5)
+        script, div = components(plot)
+
+        return render_template('batch_dow_by_dow.html', details=details, current_dow = int(dow), dows = dows, script=script, div=div)
+    else:
+        cur.close()
+
+@app.route('/ventra/batch_result/dayofweek_by_dow2/', methods = ['GET','POST'])
+def batch_dayofweek_by_dow2():
+    dows = list(range(7))
+    dows2 = list(range(7))
+    dow = request.args.get('dow')
+    dow2=request.args.get('dow2')
+    if dow == None:
+        dow = '0'
+    if dow2==None:
+        dow2='0'
+
+    df = pd.read_sql("select * from Data_2015_Ver1_dayofweek where dayofweek = " + dow, con=mysql.connection)
+    df2 = pd.read_sql("select * from Data_2015_Ver1_dayofweek where dayofweek = " + dow2, con=mysql.connection)
+
+    df.iloc[:,-1] = df.iloc[:,-1].div(52)
+    df2.iloc[:,-1] = df2.iloc[:,-1].div(52)
+
+    if dow=='3':
+        df.iloc[:, -1] = df.iloc[:, -1].multiply(52).div(53)
+    if dow2=='3':
+        df2.iloc[:, -1] = df2.iloc[:, -1].multiply(52).div(53)
+
+    source = ColumnDataSource(df)
+    source2 = ColumnDataSource(df2)
+
+    plot = figure(plot_width=500, plot_height=400,x_axis_label='hour', y_axis_label='Average Count',y_range=(0, 10000),x_range=(-0.5, 23.5))
+
+    plot.vbar(x='hour', source=source, width=1, top='count',fill_color="deeppink",fill_alpha=0.5,line_color="deeppink")
+    plot.vbar(x='hour', source=source2, width=1, top='count',fill_color="gold",fill_alpha=0.5,line_color="gold")
+
+    script, div = components(plot)
+    return render_template('batch_dow_by_dow2.html', current_dow = int(dow), current_dow2 = int(dow2), dows=dows,dows2=dows2,script=script, div=div)
+
+
+
+@app.route('/ventra/batch_result/user/',methods=['GET'] )
+def batch_user():
+    cur = mysql.connection.cursor()
+    resultvalue = cur.execute("select * from Data_2015_Ver1_user order by count desc limit 30")
+
+    if resultvalue:
+        details = cur.fetchall()
+        cur.close()
+        return render_template('batch_hour_usr.html', details=details)
+    else:
+        cur.close()
+
+@app.route('/ventra/batch_result/user_by_hour/', methods = ['GET', 'POST'])
+def batch_user_by_hour():
+    hours = list(range(24))
+    hour = request.args.get('hour')
+    if hour == None:
+        hour = '0'
+    cur = mysql.connection.cursor()
+    resultvalue = cur.execute("select * from Data_2015_Ver1_user where hour = " + hour+" order by count desc limit 20")
+    if resultvalue:
+        details = cur.fetchall()
+        cur.close()
+        df = pd.read_sql("SELECT * FROM Data_2015_Ver1_user WHERE hour = " + hour+" order by count desc limit 20",con=mysql.connection)
+        #df['index']=df.index
+        source = ColumnDataSource(df)
+        plot = figure(y_axis_type='log',plot_width=500, plot_height=400, y_axis_label='Count',y_range=(1, 3000),x_range=(-0.5, 19.5))
+        plot.vbar(x='index', source=source, width=1, top='count', bottom=1,fill_alpha=0.5,fill_color="deeppink")
+        script, div = components(plot)
+        return render_template('batch_user_by_hour.html', details=details, current_hour = int(hour), hours = hours, script=script, div=div)
+    else:
+        cur.close()
+
+
+
+@app.route('/ventra/stream_result/data/', methods=['POST'])
+def data():
+    r = redis.StrictRedis(host='ec2-54-82-188-230.compute-1.amazonaws.com', port=6379, db=0)
+    year = int(r.get('year'))
+    month = int(r.get('month'))
+    hour = int(r.get('hour'))
+    date = int(r.get('date'))
+    minn = int(r.get('min'))
+    secc = int(r.get('sec'))
+    count = int(r.get('counting'))
+    seconds = int(time.mktime(datetime.datetime(year, month, date, hour, minn, secc).timetuple()) - time.mktime(datetime.datetime(2011, 1, 1, 0, 0, 0).timetuple()))
+    print('year: ' + str(year) + ' month: ' + str(month) + ' date: ' + str(date) + ' hour: ' + str(hour) + ' min: ' + str(minn) + ' sec: ' + str(secc) + ' count: ' + str(count) + ' seconds: ' + str(seconds))
+    return jsonify(x = [seconds], y = [count])
+
+
+@app.route('/ventra/stream_result/',methods=['GET'])
+def streaming():
+    source = AjaxDataSource(data_url = request.url_root + 'ventra/stream_result/data/', polling_interval = 1000, mode = 'append')
+    #plot = figure(plot_width=300, plot_height=300)
+    plot = figure(plot_height=300,sizing_mode='scale_width')
+    plot.line('x', 'y', source = source, line_width = 4)
+    script, div = components(plot)
+    return render_template('seconds.html', script = script, div = div)
+
+
+"""
--- src/batch_processing/spark_batch3.py
+++ src/batch_processing/spark_batch3.py
@@ -8,89 +8,83 @@ import datetime
 def get_data(json_record):
     try:
         json_body = json.loads(json_record)
-	sender_id = int(json_body['actor']['id'])
-	receiver_id=int(json_body['transactions'][0]['target']['id'])
-	#timestamp = json_body['created_time']
-	#year = int(timestamp[0:4])
-	#month = int(timestamp[5:7])
-	#date = int(timestamp[8:10])
-	#hour = int(timestamp[11:13])
-	#dayofweek = int(datetime.datetime(year, month, date).strftime('%w'))
-	return ((sender_id,receiver_id), 1)
+        sender_id = int(json_body['actor']['id'])
+        receiver_id=int(json_body['transactions'][0]['target']['id'])
+        timestamp = json_body['created_time']
+        year = int(timestamp[0:4])
+	    return ((year,sender_id,receiver_id),1)
     except:
-	return None
+        return None
 
 db_config = {
     'host': 'ec2-54-82-188-230.compute-1.amazonaws.com',
     'user': 'nayoon',
     'password': 'haonayoon',
-    'database': 'insight_data_test'}
+    'database': 'insight_data_user'}
 
-#((sender_id,receiver_id), 1)
+#((year,sender_id,receiver_id), 1)
 
-
-data_schema2 = """CREATE TABLE IF NOT EXISTS Data_pairs_testpartition (
+data_schema2 = """CREATE TABLE IF NOT EXISTS Data_pairs_test(
+		year INT,
 		sender_id INT,
 		receiver_id INT,
 		count INT,
 		PRIMARY KEY (sender_id,receiver_id)
 		);
 	     """
-
-	#stmt1 = """ insert ignore into Data_year_dayofweek_hour (year,dayofweek,hour,count) VALUES (%s,%s,%s,%s);"""
-stmt2 = """ insert ignore into Data_pairs_testpartition (sender_id,receiver_id,count) VALUES (%s,%s,%s);"""
-	#stmt3 = """ insert ignore into Data_dayofweek_hour (dayofweek,hour,count) VALUES (%s,%s,%s);"""
-	#stmt4 = """ insert ignore into Data_hour (hour,count) VALUES (%s,%s);"""
+#stmt1 = """ insert ignore into Data_year_dayofweek_hour (year,dayofweek,hour,count) VALUES (%s,%s,%s,%s);"""
+stmt2 = """insert ignore into Data_pairs_test (year,sender_id,receiver_id,count) VALUES (%s,%s,%s);"""
+#stmt3 = """ insert ignore into Data_dayofweek_hour (dayofweek,hour,count) VALUES (%s,%s,%s);"""
+#stmt4 = """ insert ignore into Data_hour (hour,count) VALUES (%s,%s);"""
 
 def sql_create_table(sql_create_table_statement):
     try:
-	connection = mysqlcon.connect(**db_config)
-	cursor = connection.cursor()
-	cursor.execute(sql_create_table_statement)
-	connection.commit()
-	cursor.close()
-	connection.close()
-	return True
+        connection = mysqlcon.connect(**db_config)
+        cursor = connection.cursor()
+        cursor.execute(sql_create_table_statement)
+	    connection.commit()
+	    cursor.close()
+	    connection.close()
+	    return True
     except Exception as e:
-	print(e)
-	return False
+	    print(e)
+	    return False
 
 def sql_insert_rdd_to_table(prepared_statement, collected_rdd):
     try:
-	connection = mysqlcon.connect(**db_config)
-	cursor = connection.cursor()
-	cursor.executemany(prepared_statement, collected_rdd)
-	connection.commit()
-	cursor.close()
-	connection.close()
-	return True
+	    connection = mysqlcon.connect(**db_config)
+	    cursor = connection.cursor()
+	    cursor.executemany(prepared_statement, collected_rdd)
+	    connection.commit()
+	    cursor.close()
+	    connection.close()
+	    return True
     except Exception as e:
-	print(e)
-	return False
+	    print(e)
+	    return False
 
 def filter_nones(data):
     if data is not None:
-	return True
+	    return True
     return False
 
 def write_pair_data(partition):
-    print('writing pair data')
     connection = mysqlcon.connect(**db_config)
     cursor = connection.cursor()
     data = []
     results = []
     for x in partition:
-	sender_id = x[0]
-	receiver_id= x[1]
-	count = x[2]
-	#print('sender_id: ' + str(sender_id + ' receiver_id: ' + receiver_id + ' count: ' + count)
-	data.append((sender_id, receiver_id,count))
-	results.append(count)
+        year=x[0]
+	    sender_id = x[1]
+	    receiver_id= x[2]
+	    count = x[3]
+	    data.append((year,sender_id, receiver_id,count))
+	    results.append(count)
     try:
-	cursor.executemany(stmt2, data)
-	connection.commit()
+	    cursor.executemany(stmt2, data)
+	    connection.commit()
     except:
-	connection.rollback()
+	    connection.rollback()
     cursor.close()
     connection.close()
     return results
@@ -103,10 +97,10 @@ def write_pair_data(partition):
 
 if __name__ == "__main__":
     sc = SparkContext(appName="Venmo")
-    read_rdd = sc.textFile("s3a://venmo-json/2011*")
+    read_rdd = sc.textFile("s3a://venmo-json/2015*")
     data_rdd = read_rdd.map(lambda x: get_data(x)).filter(lambda x: filter_nones(x))
     #((year,dayofweek,hour,sender_id, receiver_id), 1)
-    data_rdd_pair=data_rdd.map(lambda rdd: ((rdd[0][0],rdd[0][1]), rdd[1])).reduceByKey(lambda a, b: a + b).map(lambda rdd:(rdd[0][0],rdd[0][1],rdd[1]))
+    data_rdd_pair=data_rdd.map(lambda rdd: ((rdd[0][0],rdd[0][1],rdd[0][2]), rdd[1])).reduceByKey(lambda a, b: a + b).map(lambda rdd:(rdd[0][0],rdd[0][1],rdd[0][2],rdd[1]))
     #year, dayofweek, hour
     #data_rdd_year_dayofweek_hour=data_rdd.map(lambda rdd: ((rdd[0][0],rdd[0][1],rdd[0][2]),rdd[1])).reduceByKey(lambda a,b:a+b).map(lambda rdd : (rdd[0][0],rdd[0][1],rdd[0][2],rdd[1]))
     #dayofweek, hour
@@ -119,31 +113,8 @@ if __name__ == "__main__":
 
     #if table_created2:
     #    data_inserted2 = sql_insert_rdd_to_table(prepared_statement=stmt2,collected_rdd=data_rdd_pair.collect())
-    print('debug')
+
     written_entries = data_rdd_pair.mapPartitions(write_pair_data)
     print(written_entries.count())
-    print('done')
-    #else:
-    #    print('Cannot create table by pair of users and isbusiness')
-    #    sys.exit(0)
-
-"""
-    if table_created1:
-        data_inserted1 = sql_insert_rdd_to_table(prepared_statement=stmt1, collected_rdd=data_rdd_year_dayofweek_hour.collect())
-    else:
-        print('Cannot create table by year,dayofweek,hour')
-        sys.exit(0)
 
 
-    if table_created3:
-        data_inserted3 = sql_insert_rdd_to_table(prepared_statement=stmt3, collected_rdd=data_rdd_dayofweek_hour.collect())
-    else:
-        print('Cannot create table dayofweek,hour')
-        sys.exit(0)
-
-    if table_created4:
-        data_inserted3 = sql_insert_rdd_to_table(prepared_statement=stmt4, collected_rdd=data_rdd_hour.collect())
-    else:
-        print('Cannot create table hour')
-        sys.exit(0)
-"""
