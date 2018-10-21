8f1e44e93cd50940ae922d61415a1392966fd3d2
nayoonwoo8899 <41222469+nayoonwoo8899@users.noreply.github.com>
2018-10-21 18:15:58 -0500

    add redis line

--- config/redis-cluster/redis-cluster.sh
+++ config/redis-cluster/redis-cluster.sh
@@ -2,7 +2,7 @@
 
 PEG_ROOT=$PEGASUS_HOME
 
-CLUSTER_NAME=NayoonWoo-IAM-redis-cluster
+CLUSTER_NAME=redis-cluster
 
 peg up redis-master.yml &
 
@@ -12,5 +12,8 @@ peg fetch ${CLUSTER_NAME}
 
 peg install ${CLUSTER_NAME} ssh
 peg install ${CLUSTER_NAME} aws
+peg install ${CLUSTER_NAME} environment
+
 peg install ${CLUSTER_NAME} zookeeper
 peg install ${CLUSTER_NAME} kafka
+peg install ${CLUSTER_NAME} redis
