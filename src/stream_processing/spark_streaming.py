842b3352c21d074e5519dcff16802d4d5d290824
nayoonwoo8899 <41222469+nayoonwoo8899@users.noreply.github.com>
2018-10-21 13:55:15 -0500

    clean spark streaming code

--- src/stream_processing/spark_streaming.py
+++ src/stream_processing/spark_streaming.py
@@ -1,3 +1,10 @@
+"""
+Spark Python script for analyzing Kafka stream and writing to Redis.
+Usage:
+    $SPARK_HOME/bin/spark-submit --jars spark-streaming-kafka-0-8-assembly_2.11-2.3.2.jar --master spark://ec2-107-23-227-201.compute-1.amazonaws.com:7077 --executor-memory 6G spark_streaming.py
+Author: Nayoon Woo (nayoonwoo8899@gmail.com)
+"""
+
 import sys
 import json
 from pyspark import SparkContext
@@ -16,40 +23,6 @@ import datetime
 import redis
 
 redis_server = 'ec2-54-82-188-230.compute-1.amazonaws.com'
-#redis_db = redis.StrictRedis(host=redis_server, port=6379, db=0)
-
-
-
-"""
-artist_list = set()
-with open('../data/lower-artist.csv', 'r') as f:
-    count =1
-    for i in f:
-        cur = i.split("\n")[0]
-        if count in range(100):
-            artist_list.add(cur)
-        count+=1
-
-def getSparkSessionInstance(sparkConf):
-    if ("sparkSessionSingletonInstance" not in globals()):
-        globals()["sparkSessionSingletonInstance"] = SparkSession.builder.config(conf=sparkConf).getOrCreate()
-    return globals()["sparkSessionSingletonInstance"]
-
-
-@udf('string')
-
-def func(text):
-    for word in artist_list:
-        if word in text.lower():
-            return word
-
-@udf('string')
-
-def transfer_time(text):
-    #return "2018-06-25"
-    return datetime.today().strftime("%Y-%m-%d %H:%M:%S")
-"""
-
 
 def process(rdd):
     if rdd.isEmpty():
@@ -75,8 +48,6 @@ def processPartition(partition):
         counting = int(redis_db.get('counting'))
 
         if prev_hour == None or prev_hour != hour:
-            print('year=', year, 'month=', month, 'date=', date, 'day of week=', dayofweek, 'hour=', hour,
-                  '# of transaction during past 1 hour=', counting)
             redis_db.set('year', year)
             redis_db.set('month', month)
             redis_db.set('date', date)
@@ -96,19 +67,6 @@ def processPartition(partition):
             redis_db.set('min', minn)
             redis_db.set('sec', secc)
             redis_db.set('counting', counting + 1)
-
-"""
-def process(rdd):
-    spark = getSparkSessionInstance(rdd.context.getConf())
-    venmoDataFrame = spark.read.json(rdd)
-    df = venmoDataFrame.withColumn('hashtag', func(tweetsDataFrame.text))
-    df = df.withColumn('time',transfer_time(tweetsDataFrame.time))
-    df.createOrReplaceTempView("historicaltweets")
-    df = spark.sql("SELECT MAX(time) AS time,hashtag, count(*) AS count FROM historicaltweets WHERE hashtag IS NOT NULL GROUP BY hashtag ORDER BY count DESC")
-    rdd = df.rdd.map(tuple)
-    rdd.saveToCassandra("twitter","tweet")
-    df.show()
-"""
 
 
 if __name__ == "__main__":
