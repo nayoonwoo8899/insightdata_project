315895ed91d73bf6b54731e52e91f5f8bf13bdd7
nayoonwoo8899 <41222469+nayoonwoo8899@users.noreply.github.com>
2018-10-21 18:17:59 -0500

    full spark cluster installation

--- config/spark-cluster/spark-cluster.sh
+++ config/spark-cluster/spark-cluster.sh
@@ -2,7 +2,7 @@
 
 PEG_ROOT=$PEGASUS_HOME
 
-CLUSTER_NAME=NayoonWoo-IAM-spark-cluster
+CLUSTER_NAME=spark-cluster
 
 peg up spark-master.yml &
 peg up spark-workers.yml &
@@ -13,5 +13,10 @@ peg fetch ${CLUSTER_NAME}
 
 peg install ${CLUSTER_NAME} ssh
 peg install ${CLUSTER_NAME} aws
+peg install ${CLUSTER_NAME} environment
+
 peg install ${CLUSTER_NAME} hadoop
 peg install ${CLUSTER_NAME} spark
+
+peg install ${CLUSTER_NAME} zookeeper
+peg install ${CLUSTER_NAME} kafka
