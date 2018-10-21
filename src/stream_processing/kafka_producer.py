ca556f43d2f6180975cab1ccc4ec3466576b1555
nayoonwoo8899 <41222469+nayoonwoo8899@users.noreply.github.com>
2018-10-21 12:12:58 -0500

    replay one day data with reverse timing order

--- src/stream_processing/kafka_producer.py
+++ src/stream_processing/kafka_producer.py
@@ -1,53 +1,62 @@
-#!/usr/bin/python
+#!/bin/env python -B
 
-# Used https://github.com/dpkp/kafka-python/blob/master/example.py as starting point
+"""
+Python script for replaying Venmo data from Amazon S3 to simulate stream processing.
+Usage:
+    kafka_producer.py [replay date]
+Author: Nayoon Woo (nayoonwoo8899@gmail.com)
+"""
 
-import botocore
-import boto3
-import threading, logging, time
+import sys
+import threading
+import time
 from kafka import KafkaProducer
+import smart_open
 
 
 class Producer(threading.Thread):
     daemon = True
+    
+    def __init__(self, date):
+        self.year = date[0:4]
+        self.month = date[4:6]
+        self.day = date[6:8]
 
     def run(self):
         producer = KafkaProducer(bootstrap_servers='localhost:9092')
 
         bucket_name = 'venmo-json'
         bucket = self.__get_s3_bucket__(bucket_name)
+        
+        # read and buffer data from S3
+        buffer = []
+        for line in smart_open.smart_open("s3://venmo-json/" + self.year + "_" + self.month + "/venmo_" + self.year + "_" + self.month + "_" + self.day +".json"):
+            buffer.append(line)
 
-        # Send data from S3 to Kafka queue
-        for obj in bucket.objects.filter():
-            data = obj.get()['Body']
-            json_body = data.read().splitlines()
-            for json_obj in json_body:
-                producer.send('venmo-transactions', json_obj)
-                time.sleep(0.1)
-                print(json_obj + '\n' + '=================================================================' + '\n')
-
-    # Access S3 bucket
-    def __get_s3_bucket__(self, bucket_name):
-        s3 = boto3.resource('s3')
-        try:
-            s3.meta.client.head_bucket(Bucket=bucket_name)
-        except botocore.exceptions.ClientError as e:
-            # If a client error is thrown, check that it was a 404 error.
-            # If it was a 404 error, then the bucket does not exist.
-            error_code = int(e.response['Error']['Code'])
-            if error_code == 404:
-                exists = False
-                print (e.response['404 Error: bucket not found'])
-            else:
-                print (e.response['Error'])
-            return None
-        else:
-            return s3.Bucket(bucket_name)
-
-
-if __name__ == "__main__":
-
-    producer = Producer()
+        # Send data from buffer to Kafka queue
+        for json_obj in reversed(buffer):
+            producer.send('venmo-transactions', json_obj)
+            time.sleep(0.1)
+            print(json_obj + '\n' + '=================================================================' + '\n')
+        
+def main():
+    
+    # read arguments
+    args = sys.argv
+    print(args)
+    if len(args) < 2:
+        print(__doc__)
+        sys.exit(1)
+        
+    # parse files
+    replay_date = args[1]
+    
+    # process data
+    producer = Producer(replay_date)
     producer.start()
     while True:
         time.sleep(10)
+    
+        
+if __name__ == '__main__':
+    main()
